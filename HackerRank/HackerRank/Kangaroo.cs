83260c0286318d8c8d42c82590e18bd69701a23b
pthieenlong <58774195+pthieenlong@users.noreply.github.com>
2020-09-15 15:48:02 +0700

    Add files via upload
    First commit

---
+++ HackerRank/HackerRank/Kangaroo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace HackerRank
+{
+    public class HackerrankKangaroo
+    {
+        public static string Kangaroo(int x1, int v1, int x2, int v2)
+        {
+            if (v1 > v2)
+            {
+                float remainder = ((x1 - x2) % (v1 - v2));
+                if (remainder == 0)
+                {
+                    return "YES";
+                }
+                else return "NO";
+            }
+            else return "NO";
+        }
+    }
+}
